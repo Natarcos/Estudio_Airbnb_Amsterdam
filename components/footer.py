06b60eacb6c341aefcf4a9869dadd8130686d224
Natarcos <n.arcos89@gmail.com>
2025-03-20 13:42:08 +0100

    footer

--- components/footer.py
+++ components/footer.py
@@ -4,10 +4,10 @@ def render_footer():
     st.markdown("---")
     st.markdown(
         """
-        <div.style='text-align:center,color:#666,'>
-        <p>Desarrollado por el Departamento de Turismo de Amsterdam y Airbnb | © 2025 Ayuntamiento de Amsterdam</p>
-            <p>¿Necesitas más información? Visita
-               <a href="https://amsterdam.org/en/">amsterdam.org</a> o
+        <div style='text-align: center; color: #666;'>
+            <p>Desarrollado por el Departamento de Turismo de Amsterdam | © 2025 Ayuntamiento de Amsterdam</p>
+            <p>¿Necesitas más información? Visita 
+               <a href="https://amsterdam.org">amsterdam.org.es</a> o 
                contacta con nuestras oficinas de turismo.
             </p>
         </div>
