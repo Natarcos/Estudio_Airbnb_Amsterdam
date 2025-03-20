50ab8946612f2a1e515494de45e98cc13508958f
Natarcos <n.arcos89@gmail.com>
2025-03-20 12:50:23 +0100

    Corrección de la ruta del archivo CSV

--- main_app_amsterdam.py
+++ main_app_amsterdam.py
@@ -8,19 +8,18 @@ from components.data_section import display_data_section
 from components.eda_section import display_eda_section
 from components.ml_section import display_ml_section
 
-
 warnings.filterwarnings('ignore')
 
 st.set_page_config(
-    page_title="🏠 Descubre Amsterdam a través de Airbnb",
+    page_title="Descubre Amsterdam a través de Airbnb",
     page_icon="🏠",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="expanded"
 )
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("/Users/n.arcos89/Documents/GitHub/Estudio_Airbnb_Amsterdam/Data/airbnbamsterdam_final_correcto.csv")
+    df = pd.read_csv("Data/airbnbamsterdam_final_correcto.csv")
     df['last_review'] = pd.to_datetime(df['last_review'])
     df['reviews_per_month'].fillna(0, inplace=True)
     df['last_review'].fillna(pd.Timestamp('1900-01-01'), inplace=True)
@@ -29,6 +28,16 @@ def load_data():
 def main():
     render_header()
     df = load_data()
+
+    # Verificar los primeros registros del DataFrame
+    st.write("Primeros registros del DataFrame:")
+    st.write(df.head())
+
+    # Verificar las columnas 'price' y 'room_type'
+    st.write("Valores únicos en 'room_type':")
+    st.write(df['room_type'].unique())
+    st.write("Valores únicos en 'price':")
+    st.write(df['price'].unique())
 
     if df is not None:
         st.sidebar.title("Navegación")
@@ -40,13 +49,13 @@ def main():
 
         choices = st.sidebar.radio("Selecciona una sección", list(pages.keys()))
         st.sidebar.markdown("---")
-        st.sidebar.markdown("### Madrid en un vistazo:")
+        st.sidebar.markdown("### Amsterdam en un vistazo:")
         st.sidebar.markdown("""
         🎭 **¿Por qué Amsterdam?**
         - Hermosos canales
-        - Cultura Vibrante
-        - Rica historia
-        - Inclusión y diversidad
+        - Cultura vibrante
+        - Rica Historia
+        - Inclusión
         """)
 
         if choices != "🤖 ML Predictor":
